efacc9f1ffd93465f9993f25ba644c7a8b2682de
G-Ilian <gbiel.boza@gmail.com>
2022-08-22 10:15:35 -0300

    Classe de modelagem da conta, usada em aula

---
+++ Aula05-Arrays/src/main/java/Conta.java
@@ -0,0 +1,10 @@
+public class Conta {
+    // Atributo da classe de conta
+    String nomeTitular; // Nome do titular
+    double saldo; // Saldo da conta
+
+    Conta(String nome,double saldo){
+        this.nomeTitular=nome;
+        this.saldo=saldo;
+    }
+}
